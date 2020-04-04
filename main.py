906d52258b2466020d635d0a53293b68ff62b04a
renato.cardoso <rgdiascardoso@gmail.com>
2020-04-04 20:56:40 +0100

    Add append func

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ def write_file(output_file, data):
     with open(output_file, 'w') as file:
         yaml.dump(data, file)
 
-def replace(param, data):
+def parse_params(params):
+    return [param.strip() for param in params.split('--')]
+
+def split_param_action(param):
+    buffer = param.split(':', 1)
+    return (buffer[0], buffer[1])
+
+def replace_value(param, data):
     parsed_param = param.split('=', 1)
     keys = parsed_param[0].split('.')
     value = parsed_param[1]
@@ -16,8 +23,13 @@ def replace(param, data):
         data = data[keys[i]]
     data[keys[-1]] = eval(value)
 
-def parse_params(params):
-    return [param.strip() for param in params.split('--replace:')]
+def append_value(param, data):
+    parsed_param = param.split('=', 1)
+    keys = parsed_param[0].split('.')
+    value = parsed_param[1]
+    for i in range(len(keys)-1):
+        data = data[keys[i]]
+    data[keys[-1]].append(eval(value))
 
 def main():
     base_path = os.environ['GITHUB_WORKSPACE']+'/'
@@ -29,7 +41,11 @@ def main():
 
     for param in parse_params(params):
         if param:
-            replace(param, data)
+            action, argument = split_param_action(param)
+            if action == 'replace':
+                replace_value(argument, data)
+            elif action == 'append':
+                append_value(param, argument)
     
     write_file(output_file, data)
 
